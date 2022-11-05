e4eb9ad580cbb06c78f08e4d5dac8766b3cff36e
Alejandro de León Fernández <aledeleonez@gmail.com>
2022-11-05 19:52:31 +0100

    Cambios

--- app/src/main/java/com/example/mistareas2/MainActivity.java
+++ app/src/main/java/com/example/mistareas2/MainActivity.java
@@ -4,16 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button ButtonPendientes;
-    private Button ButtonHechas;
+    private Button mButtonPendientes;
+    private Button mButtonHechas;
+    private Button mButtonMas;
+    private ListView mListaTareas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
+        mButtonPendientes = (Button) findViewById(R.id.buttonPendientes);
+        mButtonHechas = (Button) findViewById(R.id.buttonHechas);
+        mListaTareas = (ListView) findViewById(R.id.listaTareas);
+
     }
 }
