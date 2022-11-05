db5c8c2efb595d8eedb562197aa3ea66c7e832e9
Alejandro de León Fernández <aledeleonez@gmail.com>
2022-11-05 19:12:49 +0100

    Layouts terminados

--- app/src/main/java/com/example/mistareas2/MainActivity.java
+++ app/src/main/java/com/example/mistareas2/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.mistareas2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button ButtonPendientes;
+    private Button ButtonHechas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
     }
 }
