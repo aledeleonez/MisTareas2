f72590a9fbbfeade8148a384ce49fba5c5dee79b
Alejandro de León Fernández <aledeleonez@gmail.com>
2022-11-06 10:59:46 +0100

    Clase NuevaTarea

--- app/src/main/java/com/example/mistareas2/NuevaTarea.java
+++ app/src/main/java/com/example/mistareas2/NuevaTarea.java
@@ -3,12 +3,28 @@ package com.example.mistareas2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class NuevaTarea extends AppCompatActivity {
+
+    private EditText mNombre;
+    private EditText mDescripcion;
+    private EditText mFecha;
+    private EditText mPrioridad;
+    private EditText mCoste;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nueva_tarea);
+
+        mNombre = findViewById(R.id.TextNombre);
+        mDescripcion = findViewById(R.id.TextDesc);
+        mFecha = findViewById(R.id.TextFecha);
+        mPrioridad = findViewById(R.id.TextPrioridad);
+        mCoste = findViewById(R.id.TextCoste);
     }
+
+    
 }
