9552a43ad120c557c866a9987cf36a7f44c7e310
Alejandro de León Fernández <aledeleonez@gmail.com>
2022-11-06 10:52:20 +0100

    Creada la clase Tarea

--- app/src/main/java/com/example/mistareas2/Tarea.java
+++ app/src/main/java/com/example/mistareas2/Tarea.java
@@ -1,14 +1,59 @@
 package com.example.mistareas2;
 
-import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+public class Tarea {
 
-public class Tarea extends AppCompatActivity {
+    private String nombre;
+    private double coste;
+    private String fecha;
+    private String prioridad;
+    private String descripcion;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tarea);
+    public Tarea(String nombre, double coste, String fecha, String prioridad, String descripcion) {
+        this.nombre = nombre;
+        this.coste = coste;
+        this.fecha = fecha;
+        this.prioridad = prioridad;
+        this.descripcion = descripcion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getCoste() {
+        return coste;
+    }
+
+    public void setCoste(double coste) {
+        this.coste = coste;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getPrioridad() {
+        return prioridad;
+    }
+
+    public void setPrioridad(String prioridad) {
+        this.prioridad = prioridad;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
     }
 }
